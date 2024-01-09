a0d9b2557e8543165e96354897193446e7035c55
oulaKhaled <126021776+oulaKhaled@users.noreply.github.com>
2024-01-09 10:58:22 +0300

    Update http-common.jsx

--- frontend-main/src/services/http-common.jsx
+++ frontend-main/src/services/http-common.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://mekan-bul-esb7.vercel.app/",
+  baseURL: "https://mekan-bul-esb7.vercel.app/api",
   headers: {
     "Accept": "application/json",
     "Content-Type": "application/json;charset=UTF-8"
